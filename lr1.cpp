6e5051df796f6dc46affc2314249c830055f8df9
Sinclairwww <90683011+Sinclairwww@users.noreply.github.com>
2023-12-11 02:35:11 +0800

    grammar

--- lr1.cpp
+++ lr1.cpp
@@ -36,9 +36,9 @@ typedef map< string, map< string, string > > analyse_table;
 class SyntaxAnalyzer {
 public:
     void loadTable ( string file_name );
-    void loadFormula ( string file_name );
+    void initFormula () { _formula = { { "E'", "E" }, { "E", "E+T" }, { "E", "E-T" }, { "E", "T" }, { "T", "T*F" }, { "T", "T/F" }, { "T", "F" }, { "F", "(E)" }, { "F", "n" } }; }
     void analyse ( string str );
-    int  startWith ( string str, string start ) { return str.find ( start ) == 0 ? 1 : 0; }
+    int  startWith ( string str, string start ) { return str.find ( start ) == 0; }
 
 private:
     analyse_table                    _table;
@@ -47,29 +47,65 @@ private:
 };
 
 void SyntaxAnalyzer::loadTable ( string file_name ) {
-    ifstream file ( file_name );
+    /*
+    state,),-,n,/,+,(,*,$,E,T,F,
+I0,0,0,s5,0,0,s4,0,0,1,2,3,
+I1,0,s7,0,0,s6,0,0,acc,0,0,0,
+I2,0,r3,0,s9,r3,0,s8,r3,0,0,0,
+I3,0,r6,0,r6,r6,0,r6,r6,0,0,0,
+I4,0,0,s14,0,0,s13,0,0,10,11,12,
+I5,0,r8,0,r8,r8,0,r8,r8,0,0,0,
+I6,0,0,s5,0,0,s4,0,0,0,15,3,
+I7,0,0,s5,0,0,s4,0,0,0,16,3,
+I8,0,0,s5,0,0,s4,0,0,0,0,17,
+I9,0,0,s5,0,0,s4,0,0,0,0,18,
+I10,s19,s21,0,0,s20,0,0,0,0,0,0,
+I11,r3,r3,0,s23,r3,0,s22,0,0,0,0,
+I12,r6,r6,0,r6,r6,0,r6,0,0,0,0,
+I13,0,0,s14,0,0,s13,0,0,24,11,12,
+I14,r8,r8,0,r8,r8,0,r8,0,0,0,0,
+I15,0,r1,0,s9,r1,0,s8,r1,0,0,0,
+I16,0,r2,0,s9,r2,0,s8,r2,0,0,0,
+I17,0,r4,0,r4,r4,0,r4,r4,0,0,0,
+I18,0,r5,0,r5,r5,0,r5,r5,0,0,0,
+I19,0,r7,0,r7,r7,0,r7,r7,0,0,0,
+I20,0,0,s14,0,0,s13,0,0,0,25,12,
+I21,0,0,s14,0,0,s13,0,0,0,26,12,
+I22,0,0,s14,0,0,s13,0,0,0,0,27,
+I23,0,0,s14,0,0,s13,0,0,0,0,28,
+I24,s29,s21,0,0,s20,0,0,0,0,0,0,
+I25,r1,r1,0,s23,r1,0,s22,0,0,0,0,
+I26,r2,r2,0,s23,r2,0,s22,0,0,0,0,
+I27,r4,r4,0,r4,r4,0,r4,0,0,0,0,
+I28,r5,r5,0,r5,r5,0,r5,0,0,0,0,
+I29,r7,r7,0,r7,r7,0,r7,0,0,0,0,
+    */
+    vector< vector< string > > input = { { "0", "0", "s5", "0", "0", "s4", "0", "0", "1", "2", "3" },      { "0", "s7", "0", "0", "s6", "0", "0", "acc", "0", "0", "0" },
+                                         { "0", "r3", "0", "s9", "r3", "0", "s8", "r3", "0", "0", "0" },   { "0", "r6", "0", "r6", "r6", "0", "r6", "r6", "0", "0", "0" },
+                                         { "0", "0", "s14", "0", "0", "s13", "0", "0", "10", "11", "12" }, { "0", "r8", "0", "r8", "r8", "0", "r8", "r8", "0", "0", "0" },
+                                         { "0", "0", "s5", "0", "0", "s4", "0", "0", "0", "15", "3" },     { "0", "0", "s5", "0", "0", "s4", "0", "0", "0", "16", "3" },
+                                         { "0", "0", "s5", "0", "0", "s4", "0", "0", "0", "0", "17" },     { "0", "0", "s5", "0", "0", "s4", "0", "0", "0", "0", "18" },
+                                         { "s19", "s21", "0", "0", "s20", "0", "0", "0", "0", "0", "0" },  { "r3", "r3", "0", "s23", "r3", "0", "s22", "0", "0", "0", "0" },
+                                         { "r6", "r6", "0", "r6", "r6", "0", "r6", "0", "0", "0", "0" },   { "0", "0", "s14", "0", "0", "s13", "0", "0", "24", "11", "12" },
+                                         { "r8", "r8", "0", "r8", "r8", "0", "r8", "0", "0", "0", "0" },   { "0", "r1", "0", "s9", "r1", "0", "s8", "r1", "0", "0", "0" },
+                                         { "0", "r2", "0", "s9", "r2", "0", "s8", "r2", "0", "0", "0" },   { "0", "r4", "0", "r4", "r4", "0", "r4", "r4", "0", "0", "0" },
+                                         { "0", "r5", "0", "r5", "r5", "0", "r5", "r5", "0", "0", "0" },   { "0", "r7", "0", "r7", "r7", "0", "r7", "r7", "0", "0", "0" },
+                                         { "0", "0", "s14", "0", "0", "s13", "0", "0", "0", "25", "12" },  { "0", "0", "s14", "0", "0", "s13", "0", "0", "0", "26", "12" },
+                                         { "0", "0", "s14", "0", "0", "s13", "0", "0", "0", "0", "27" },   { "0", "0", "s14", "0", "0", "s13", "0", "0", "0", "0", "28" },
+                                         { "s29", "s21", "0", "0", "s20", "0", "0", "0", "0", "0", "0" },  { "r1", "r1", "0", "s23", "r1", "0", "s22", "0", "0", "0", "0" },
+                                         { "r2", "r2", "0", "s23", "r2", "0", "s22", "0", "0", "0", "0" }, { "r4", "r4", "0", "r4", "r4", "0", "r4", "0", "0", "0", "0" },
+                                         { "r5", "r5", "0", "r5", "r5", "0", "r5", "0", "0", "0", "0" },   { "r7", "r7", "0", "r7", "r7", "0", "r7", "0", "0", "0", "0" } };
+    ifstream                   file ( file_name );
     if ( !file.is_open () ) {
         cout << "Error: file " << file_name << " not found" << endl;
         return;
     }
     analyse_table    table;
     string           line;
-    vector< string > tokens;
+    vector< string > tokens = { ")", "-", "n", "/", "+", "(", "*", "$", "E", "T", "F" };
     getline ( file, line );
     string word;
-    for ( int i = 0; i < line.size (); i++ ) {
-        if ( line[ i ] == ',' || line[ i ] == '\n' ) {
-            string str2 = "state";
-            if ( word.size () > 0 && word.compare ( str2 ) != 0 ) {
-                tokens.push_back ( word );
-                word.clear ();
-            } else {
-                word.clear ();
-            }
-        } else {
-            word += line[ i ];
-        }
-    }
+
     while ( getline ( file, line ) ) {
         string word;
         int    count = 0;
@@ -96,29 +132,6 @@ void SyntaxAnalyzer::loadTable ( string file_name ) {
     _table = table;
 }
 
-void SyntaxAnalyzer::loadFormula ( string file_name ) {
-    ifstream file ( file_name );
-    if ( !file.is_open () ) {
-        cout << "Error: file " << file_name << " not found" << endl;
-        return;
-    }
-    vector< pair< string, string > > formula;
-    string                           line;
-    while ( getline ( file, line ) ) {
-        string word[ 2 ];
-        int    count = 0;
-        for ( int i = 0; i < line.size (); i++ ) {
-            if ( line[ i ] == '-' && i + 1 < line.size () && line[ i + 1 ] == '>' ) {
-                count++;
-                i++;
-            } else {
-                word[ count ] += line[ i ];
-            }
-        }
-        formula.push_back ( make_pair ( word[ 0 ], word[ 1 ] ) );
-    }
-    _formula = formula;
-}
 
 void SyntaxAnalyzer::analyse ( string str ) {
     int              ip = 0;
@@ -197,7 +210,6 @@ int main () {
     init_index ();
     SyntaxAnalyzer analyzer;
     analyzer.loadTable ( "lr1.csv" );
-    analyzer.loadFormula ( "lr1.conf" );
     string str;
     cin >> str;
     str = process ( str );
@@ -206,4 +218,4 @@ int main () {
 }
 
 
-// 改读入文件
+// 改csv的读入
